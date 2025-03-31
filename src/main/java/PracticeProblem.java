787a2606e708fbbcf355509c57e4583d77a31ad5
cindy pan <cindy.pan@student.tdsb.on.ca>
2025-03-31 03:24:35 +0000

    Update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,43 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static void selectionSortName(String[] names, int[] ages) {
+	for (int out=0; out<names.length-1; out++){
+		int min = out;
 
+		for (int in=out+1; in<names.length; in++){
+			if (names[in].compareToIgnoreCase(names[min])<0){
+				min = in;
+			}
+
+		}
+		String temp = names[out];
+		names[out]=names[min];
+		names[min]= temp;
+
+		int tempInt = ages[out];
+		ages[out]=ages[min];
+		ages[min]=tempInt;
 	}
+}
 
+	public static void selectionSortAge(String[] names, int[] ages) {
+		for (int out=0; out<names.length-1; out++){
+			int min = out;
 	
+			for (int in=out+1; in<names.length; in++){
+				if (ages[in]<ages[min]){
+					min = in;
+				}
+	
+			}
+			String temp = names[out];
+			names[out]=names[min];
+			names[min]= temp;
+	
+			int tempInt = ages[out];
+			ages[out]=ages[min];
+			ages[min]=tempInt;
+		}
+}
 
 }
